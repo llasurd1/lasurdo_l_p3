2a90acbad78e3047466808370130a488cece2762
Liam Lasurdo <38768478+llasurd1@users.noreply.github.com>
2020-10-26 14:31:28 -0400

    Update submission.cpp

--- submission.cpp
+++ submission.cpp
@@ -46,7 +46,9 @@ int main(int argc, char *argv[]) {
 	}
 	if(alg==0) {
 		for(int i = 0; i<set-1; i++) {
-			for int j = 0; j<	
+			for int j = 0; j<10; j++) {
+				
+			}
 		}
 	}
 	else if(alg==1) {

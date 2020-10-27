cce7d05f05d99874a4c8e2e5a85e9425ba4d3e47
Liam Lasurdo <38768478+llasurd1@users.noreply.github.com>
2020-10-27 17:05:29 -0400

    Update submission.cpp

--- submission.cpp
+++ submission.cpp
@@ -204,6 +204,9 @@ int main(int argc, char *argv[]) {
 					}
 				}
 			}
+			for(int t = 0; t<10; t++) {
+				//cout << ratios[u][t] << " " << weights[u][t] << " " << profits[u][t]<< endl;
+			}
 			int bestset[1000];
 			int include[1000];
 			int numbest = 0;
